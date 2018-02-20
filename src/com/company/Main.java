a17c7b74a06d4367a36b808be7ea4e5aa25045c2
Mantas <mantas.muzzz@gmail.com>
2018-02-20 10:34:07 +0200

    padariau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,13 +14,18 @@ public class Main {
         Scanner in = new Scanner(System.in);
         double mase, ugis;
         double kmi;
+        Main obj = new Main();
 
         System.out.print("Iveskite mase: ");
         mase = in.nextDouble();
         System.out.print("Iveskite ugi: ");
         ugis = in.nextDouble();
 
-        kmi = mase / (Math.pow(ugis, 2));
+        kmi = obj.kmi(mase, ugis);
         System.out.println("KMI: " + kmi);
+    }
+
+    public double kmi(double m, double u){
+        return m / (Math.pow(u, 2));
     }
 }
